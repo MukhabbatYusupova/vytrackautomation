fca5b225b61bf9d27717115a0a50d5c60cb658c1
inneska_93 <klybanivska@gmail.com>
2019-08-15 19:19:33 -0400

    another commit

--- src/test/java/com/vytrack/tests/smoke_tests/InnaClass.java
+++ src/test/java/com/vytrack/tests/smoke_tests/InnaClass.java
@@ -3,5 +3,6 @@ package com.vytrack.tests.smoke_tests;
 public class InnaClass {
     public static void main(String[] args) {
         System.out.println("New");
+        System.out.println("something new");
     }
 }
